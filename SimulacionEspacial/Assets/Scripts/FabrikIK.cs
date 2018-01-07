321b5b4569375ef0226f4be1dac2ea81e6782ca5
Adrià Biarnés Belso <adriabiarnesbelso@enti.cat>
2018-01-07 23:52:04 +0100

    Canvis
    No funciona. No surt.
    Fa coses rares i no va

--- SimulacionEspacial/Assets/Scripts/FabrikIK.cs
+++ SimulacionEspacial/Assets/Scripts/FabrikIK.cs
@@ -6,13 +6,16 @@ using UnityEngine;
 public class FabrikIK : MonoBehaviour
 {
     public Transform[] joints;
+    public Transform arm0;
     public Transform target;
     //testing
-    public Transform projection;
-    public Transform anotherProjection;
+    public Transform projection;        //DEBUG
+    public Transform anotherProjection; //DEBUG
+    public Transform[] copyDEBUG;        //DEBUG
     //public Transform plane, plane2;
     public Transform[] planes;
-    public Quaternion[] planCopyRotations;
+    public Quaternion[] planeCopyRotations;     //...............potser s'haurà d'esborrar
+    public Vector3[] planeNormals;
 
     private Vector3[] copy;
     private float[] distances;
@@ -27,8 +30,8 @@ public class FabrikIK : MonoBehaviour
     {
         distances = new float[joints.Length - 1];
         copy = new Vector3[joints.Length];
-        planCopyRotations = new Quaternion[planes.Length];
-        //-----------------------------------------------guardar les rotacions originals dels plans? (planeCopyRotations)-----------------
+        planeCopyRotations = new Quaternion[planes.Length];
+        planeNormals = new Vector3[planes.Length];
     }
 
     void Update()
@@ -47,7 +50,8 @@ public class FabrikIK : MonoBehaviour
         //copy plane rotations
         for (int i = 0; i < planes.Length; i++)
         {
-            planCopyRotations[i] = planes[i].rotation;
+            planeCopyRotations[i] = planes[i].rotation;
+            planeNormals[i] = planes[i].up;
         }
 
         done = (Vector3.Distance(target.position, joints[joints.Length - 1].position) < threshold_distance);
@@ -97,7 +101,7 @@ public class FabrikIK : MonoBehaviour
             }
 
             //actualitzar rotacions plans
-            updatePlaneRotations();
+            updatePlaneCopyRotations();
 
             //---------------------CONSTRAINTS-------------------
             Vector3 vectorToPlane;
@@ -108,10 +112,12 @@ public class FabrikIK : MonoBehaviour
             for (int i = 1; i <= 3; i++)     //joints que necessiten constraints
             {
                 //Projectem el punt del joint al pla
-                vectorToPlane = -planes[i-1].up;                                            //vector com la normal del pla, en direcció al pla
+                //vectorToPlane = -planes[i - 1].up;                                            //vector com la normal del pla, en direcció al pla--------------------------------
+                vectorToPlane = -planeNormals[i-1];                                            //vector com la normal del pla, en direcció al pla--------------------------------
                 pointInLine = copy[i] + vectorToPlane;
-                escalar = Vector3.Dot(planes[i-1].up.normalized, (copy[i-1] - copy[i])) /
-                    (Vector3.Dot(planes[i-1].up.normalized, (pointInLine - copy[i])));      //copy[1]->punt en el pla
+                escalar = Vector3.Dot(planeNormals[i - 1].normalized, (copy[i - 1] - copy[i])) /(Vector3.Dot(planeNormals[i - 1].normalized, (pointInLine - copy[i])));      //copy[1]->punt en el pla----------
+                //escalar = Vector3.Dot(planes[i-1].up.normalized, (copy[i-1] - copy[i])) /
+                    //(Vector3.Dot(planes[i-1].up.normalized, (pointInLine - copy[i])));      //copy[1]->punt en el pla
                 projectedPoint = copy[i] + escalar * vectorToPlane;                         //sense tenir en compte les distancies
                 if (escalar != 0)
                 {
@@ -124,37 +130,65 @@ public class FabrikIK : MonoBehaviour
             //---------------------END CONSTRAINTS-------------------
 
             updateJointRotations();
-
+            //updatePlaneRotations
         }
     }
 
     //actualitza la rotació del pla segons les posicions dels copy
-    void updatePlaneRotations()
+    void updatePlaneCopyRotations()
     {
         //plane 1
-        //buscar la projecció dels joint 1 i el copy 1 al pla 
+        //buscar la projecció dels joint 1 i el copy 1 al pla
+        Vector3 jointProjection = getProjectionToPlane(Vector3.up, joints[0].position, joints[1].position);
+        Vector3 copyProjection = getProjectionToPlane(Vector3.up, joints[0].position, copy[1]);
 
-        /*Vector3 vectorToPlane = -Vector3.up;    //només funcionarà si la nau no està girada
-        Vector3 pointInLine = copy[1] + vectorToPlane;
-        float escalar = Vector3.Dot(Vector3.up, (copy[0] - copy[1])) /
-                    (Vector3.Dot(Vector3.up, (pointInLine - copy[1])));
-        Vector3 projectedPoint = copy[1] + escalar * vectorToPlane;
-        Vector3 projection = copy[1];
-        if (escalar != 0)
-        {
-            projection = copy[0] + (projectedPoint - copy[0]).normalized * distances[0];
-        }*/
-        Vector3 jointProjection = getProjectionToPlane(Vector3.up, joints[0].position, joints[1].position); //VIGILAR SI FAIG SERVIR EL COPY O EL JOINT (EL COPY DESPRES S'ACABA MODIFICANT)
-        Vector3 copyProjection = getProjectionToPlane(Vector3.up, joints[0].position, copy[1]); //VIGILAR SI FAIG SERVIR EL COPY O EL JOINT (EL COPY DESPRES S'ACABA MODIFICANT)
-
-        anotherProjection.position = copyProjection;
+        //anotherProjection.position = copyProjection;    //------------------------------------------------------------------------------DEBUG
 
         //buscar la rotació en l'eix Y entre copy[0] i copy[1]
         Quaternion planeRotation = getRotationFrom(joints[0].position, joints[0].position, jointProjection, copyProjection);
-
+        arm0.rotation = planeRotation * arm0.rotation;
         //guardar la rotació 
         //planCopyRotations[i] = 
-        planes[0].rotation = planeRotation * planes[0].rotation;
+        planeCopyRotations[0] = planeRotation * planes[0].rotation;                         //PER ESBORRAR SEGURAMENT-------------------------------------
+        planeCopyRotations[1] = planeRotation * planes[1].rotation;
+
+        //actualitzar rotacio plane 3
+        //hem d'obtenir el vector de la normal del pla2 i rotarlo
+
+        //DEBUG
+        Debug.DrawRay(joints[0].position, planeNormals[0] * 30, Color.blue);
+        Debug.DrawRay(joints[2].position, planeNormals[1] * 30, Color.blue);
+        Debug.DrawRay(joints[3].position, planeNormals[2] * 30, Color.blue);
+
+        planeNormals[0] = rotateVecWithQuat(planeNormals[0], planeRotation);
+        planeNormals[1] = rotateVecWithQuat(planeNormals[1], planeRotation);
+
+        //Necessitem trasladar els punts joints[2] i joints[3] per tal de poder projectar correctament els punts
+        Vector3 translateVec = copy[2] - joints[2].position; //vector per trasladar joints[2] a copy[2]
+        Vector3 joint3Translated = joints[3].position + translateVec;
+        copyDEBUG[5].position = joint3Translated;
+
+        Debug.DrawRay(joint3Translated, copy[2] - joint3Translated, Color.white);
+        
+        Vector3 joint3projec = getProjectionToPlane(planeNormals[1], copy[2], joint3Translated); //DONA PROBLEMES - DONA INFINIT
+        Vector3 copy3projec = getProjectionToPlane(planeNormals[1], copy[2], copy[3]);
+        Quaternion plane3VertRot = getRotationFrom(copy[2], copy[2], joint3projec, copy3projec);
+
+        print(joint3projec);
+        print(copy3projec);
+
+        planeNormals[2] = rotateVecWithQuat(planeNormals[2], planeRotation * plane3VertRot);
+
+        for (int i = 0; i < copy.Length; i++)
+        {
+            copyDEBUG[i].position = copy[i];
+        }
+
+        //DEBUG
+        Debug.DrawRay(copy[0], planeNormals[0] * 30, Color.red);
+        Debug.DrawRay(copy[2], planeNormals[1] * 30, Color.red);
+        Debug.DrawRay(copy[3], planeNormals[2] * 30, Color.red);
+
     }
 
     //Rotació entre dos punts amb un punt d'origen donat
@@ -167,7 +201,7 @@ public class FabrikIK : MonoBehaviour
         Vector3 a = pointA - originA;
         Vector3 b = pointB - originB;
         Vector3 axis = Vector3.Cross(a, b).normalized;
-
+        
         float cosa = Vector3.Dot(a, b) / (a.magnitude * b.magnitude);
         float sina = Vector3.Cross(a.normalized, b.normalized).magnitude;
         float alpha = Mathf.Atan2(sina, cosa);
@@ -176,16 +210,17 @@ public class FabrikIK : MonoBehaviour
     }
 
 
-    //Projeccio del punt al pla (retorna el mateix punt si ja estava al pla)
+    //Projeccio del punt al pla (retorna el mateix punt si ja estava al pla) sense tenir en compte les distances (la distancia fins el pointInPlane pot ser qualsevol)
+    //http://geomalgorithms.com/a05-_intersect-1.html
     Vector3 getProjectionToPlane(Vector3 planeNormal, Vector3 pointInPlane, Vector3 pointToProject)
     {
-        Vector3 pointInLine = pointToProject - planeNormal;
-        float escalar = Vector3.Dot(Vector3.up.normalized, (pointInPlane - pointToProject)) /
-                    (Vector3.Dot(Vector3.up.normalized, (pointInLine - pointToProject)));
+        Vector3 pointInLine = pointToProject - planeNormal;         //--------------------------segurament l'error esta en el Vector3.up
+        float escalar = Vector3.Dot(planeNormal.normalized, (pointInPlane - pointToProject)) /
+                    (Vector3.Dot(planeNormal.normalized, (pointInLine - pointToProject)));
         return (pointToProject + escalar * (-planeNormal));
     }
 
-    // Update all original joint rotations
+    // Update all original joint rotations and positions
     void updateJointRotations()
     {
         for (int i = 0; i <= joints.Length - 2; i++)
@@ -209,6 +244,13 @@ public class FabrikIK : MonoBehaviour
         }
     }
 
+    void updatePlaneRotations()//---------------------------------------------------------------------------DELETE (i el atribut plane rotations)
+    {
+        for (int i = 0; i <= 0; i++)    //CANVIAR PER planes.Lenght
+        {
+            planes[i].rotation = planeCopyRotations[i];
+        }
+    }
     //recol·loquem la resta de nodes (començant per el startNode)
     void repositionCopyNodes(int startNode)
     {
@@ -237,5 +279,12 @@ public class FabrikIK : MonoBehaviour
     //Actualitza la rotació del primer (no està dintre  joint de tots (només és un twist)
     void updateTwistJoint() {
 
+    }
+
+    Vector3 rotateVecWithQuat(Vector3 vec, Quaternion rotation)
+    {
+        Quaternion vecQuat = new Quaternion(vec.x, vec.y, vec.z, 0);
+        Quaternion result = rotation * vecQuat * Quaternion.Inverse(rotation);
+        return new Vector3(result.x, result.y, result.z);
     }
 }
